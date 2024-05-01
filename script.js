029720e7b9e5c9078b014cd1b417204a47194c08
Nando Soares <130779847+NandoBarsoaf@users.noreply.github.com>
2024-04-30 22:51:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  let buttons = document.querySelectorAll('.btn');
+  let visor = document.querySelector('.visor');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', function () {
+      let buttonValue = this.textContent;
+      visor.textContent = buttonValue;
+    });
+  });
+});
